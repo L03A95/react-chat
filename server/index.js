4925ac965f1b617e2422a2d30c63ddd07f111e75
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-08-30 06:10:44 -0300

    Added active users functionality

--- server/index.js
+++ server/index.js
@@ -6,11 +6,22 @@ const io = require('socket.io')(server, {
     cors: { origin: "*" }
 })
 
+let usuarios = []
+
 io.on('connection', (socket) => {
 
     socket.on('connected', (data) => {
-        console.log('Se ha conectado ' + data.user)
+        usuarios.push(data.user)
+        console.log(usuarios)
+        io.emit('connected', {user: data.user, users: usuarios})
     })
+
+    socket.on('disconected', (data) => {
+        usuarios = usuarios.filter((user) => user !== data.user)
+        console.log(usuarios)
+        io.emit('disconected', {user: data.user, users: usuarios})
+    })
+
     socket.on('chat_message', (data) => {
         io.emit('chat_message', data)
     })
